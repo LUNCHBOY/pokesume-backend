af819f43391772e09e96f65f53e18fa4a4f248f6
LUNCHBOY <67341315+LUNCHBOY@users.noreply.github.com>
2025-11-19 23:27:27 -0800

    Update database.js

--- config/database.js
+++ config/database.js
@@ -2,14 +2,22 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === 'production' ? {
+  ssl: {
     rejectUnauthorized: false
-  } : false
+  },
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 10000  // Increased to 10 seconds
 });
 
 pool.on('error', (err) => {
   console.error('Unexpected error on idle client', err);
-  process.exit(-1);
+  // Don't exit, just log
+  console.error('Database error occurred, but server will continue');
+});
+
+pool.on('connect', () => {
+  console.log('[DB] Connected to PostgreSQL');
 });
 
 module.exports = {
